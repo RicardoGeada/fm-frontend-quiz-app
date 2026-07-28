0438359e100c4ab18409f6a2e72d0d064eb664fb
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2026-07-28 11:15:52 +0200

    feat: add start page with react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Start from './pages/Start'
 
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path='/' element={<Start />} />
+    </Routes>
   )
 }
 
