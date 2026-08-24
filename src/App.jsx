c5403b0f6b5c2248bd4a26ae53680009d9556b1e
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2026-08-24 09:36:24 +0200

    feat(quiz-page): add quiz page with basic html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Start from './pages/Start/Start'
+import Quiz from './pages/Quiz/Quiz'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Start />} />
+      <Route path='/quiz' element={<Quiz />} />
     </Routes>
   )
 }
---
+++ src/pages/Quiz/Quiz.jsx
@@ -0,0 +1,32 @@
+import "./Quiz.css";
+import Header from "../../components/Header/Header";
+
+function Quiz() {
+  return (
+    <div className="quiz-page">
+      <div className="quiz-page__content">
+        <Header />
+        <main className="quiz">
+          <section className="quiz__question-section">
+            <div className="quiz__question-content">
+              <p className="quiz__progress-text text-preset-5">
+                Question 6 of 10
+              </p>
+
+              <p className="quiz__question text-preset-3">
+                Which of these color contrast ratios defines the minimum WCAG
+                2.1 Level AA requirement for normal text?
+              </p>
+            </div>
+
+            <div className="quiz__progress-bar"></div>
+          </section>
+
+          <form className="quiz__form">{/* Antworten */}</form>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Quiz;
