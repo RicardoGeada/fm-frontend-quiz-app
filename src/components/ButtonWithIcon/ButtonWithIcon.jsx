ae0d05e4cdc7dae3ae9f244046a7fc2e7f50077e
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2026-08-24 08:13:58 +0200

    feat(): add quiz selection buttons with styling

--- src/components/ButtonWithIcon/ButtonWithIcon.jsx
+++ src/components/ButtonWithIcon/ButtonWithIcon.jsx
@@ -1,14 +1,17 @@
 import "./ButtonWithIcon.css";
-import icon from "../../assets/images/icon-html.svg";
 
-function ButtonWithIcon() {
+
+function ButtonWithIcon({ icon, iconBackgroundColor, text }) {
   return (
     <button className="button-with-icon text-preset-4">
-      <div className="button-with-icon__image" style={{backgroundColor: "var(--color-orange-50)"}}>
+      <div
+        className="button-with-icon__image"
+        style={{ backgroundColor: iconBackgroundColor }}
+      >
         <img src={icon} alt="" />
       </div>
 
-      <span>HTML</span>
+      <span>{text}</span>
     </button>
   );
 }
--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -1,13 +1,17 @@
 import "./Start.css";
 import Header from "../../components/Header/Header";
 import ButtonWithIcon from "../../components/ButtonWithIcon/ButtonWithIcon";
+import html from "../../assets/images/icon-html.svg";
+import css from "../../assets/images/icon-css.svg";
+import javascript from "../../assets/images/icon-js.svg";
+import accessibillity from "../../assets/images/icon-accessibility.svg";
 
 function Start() {
   return (
     <div className="page">
       <div className="page__content">
         <Header />
-        <main>
+        <main className="page__main">
           <div className="headline">
             <h1 className="headline__title text-preset-2">
               <span className="text-preset-2--light">Welcome to the</span>
@@ -16,7 +20,10 @@ function Start() {
             <span className="headline__subtitle text-preset-5">Pick a subject to get started.</span>
           </div>
           <div className="quiz-buttons">
-            <ButtonWithIcon />
+            <ButtonWithIcon icon={html} iconBackgroundColor={"var(--color-orange-50)"} text={"HTML"}/>
+            <ButtonWithIcon icon={css} iconBackgroundColor={"var(--color-green-100)"} text={"CSS"}/>
+            <ButtonWithIcon icon={javascript} iconBackgroundColor={"var(--color-blue-50)"} text={"Javascript"}/>
+            <ButtonWithIcon icon={accessibillity} iconBackgroundColor={"var(--color-purple-100)"} text={"Accessebility"}/>
           </div>
         </main>
       </div>
